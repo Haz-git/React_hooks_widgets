30f8250b62885b288e5f231ffd9118d92fccf66f
Haz-git <zhouharry1@gmail.com>
2020-09-02 09:23:43 -0700

    Added Listing Retrievals

--- src/components/Search.js
+++ src/components/Search.js
@@ -27,12 +27,12 @@ const Search = () => {
 
     const renderedResults = results.map((result) => {
         return (
-            <div className="item">
+            <div key={result.pageid} className="item">
                 <div className="content">
                     <div className="header">
                         {result.title}
                     </div>
-                    {result.snippet}
+                    <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
                 </div>
             </div>
         )
