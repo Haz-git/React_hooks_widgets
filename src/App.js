002329862a121699ba83f5ca14c601b9a50bab0c
Haz-git <zhouharry1@gmail.com>
2020-09-02 10:42:56 -0700

    Adding new widget -- dropdown menu

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
+import Dropdown from './components/Dropdown';
 
 const items = [
     {
@@ -18,9 +19,25 @@ const items = [
 
 ]
 
+const options = [
+    {
+        label: 'The Color Red',
+        value: 'red'
+    },
+    {
+        label: 'The Color Blue',
+        value: 'blue'
+    },
+    {
+        label: 'The Color Green',
+        value: 'green'
+    }
+]
+
 export default () => {
     return (
         // <Accordion items={items} />
-        <Search />
+        // <Search />
+        <Dropdown options={options}/>
     )
 }
