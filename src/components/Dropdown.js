d3e572fddb5159c92abd9c05ae44e6f7418979a8
Haz-git <zhouharry1@gmail.com>
2020-09-02 15:11:18 -0700

    Added More Dropdown functionality

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Dropdown = ({ options }) => {
+const Dropdown = ({ options, selected, onSelectedChange }) => {
+
+    const [open, setOpen] = useState(false);
 
     const renderedOptions = options.map((option) => {
+        if (option.value === selected.value) {
+            return null;
+        }
+
         return (
-            <div key={option.value} className="item">
+            <div 
+                key={option.value}  
+                className="item"
+                onClick={() => onSelectedChange(option)}
+            >
                 {option.label}
             </div>
         )
@@ -13,10 +23,10 @@ const Dropdown = ({ options }) => {
         <div className="ui form">
             <div className="field">
                 <label className="label">Temporary color</label>
-                <div className="ui selection dropdown visible active">
+                <div onClick={() => setOpen(!open)} className={`ui selection dropdown ${open ? 'visible active' : ''}`}>
                     <i className="dropdown icon"></i>
-                    <div className="text">Temporary Color select</div>
-                    <div className="menu visible transition">
+                    <div className="text">{selected.label}</div>
+                    <div className={`menu ${open ? 'visible transition' : ''}`}>
                         {renderedOptions}
                     </div>
                 </div>
